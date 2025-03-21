8332294f6b0458a6256321291b5d145e366c9b6b
Ruchithamgowda <ruchithagowda067@gmail.com>
2025-03-21 15:33:33 +0530

    pushing all the code to git

---
+++ ShoeBrands.java
@@ -0,0 +1,114 @@
+class ShoeBrands{
+    public static void main(String[] args) {
+        // Defining 24 Indian shoe brands
+        String shoe1 = "Bata";
+        String shoe2 = "Paragon";
+        String shoe3 = "Relaxo";
+        String shoe4 = "Liberty";
+        String shoe5 = "Woodland";
+        String shoe6 = "Lancer";
+        String shoe7 = "Red Chief";
+        String shoe8 = "Metro";
+        String shoe9 = "Khadim's";
+        String shoe10 = "Mochi";
+        String shoe11 = "Lee Cooper";
+        String shoe12 = "Action";
+        String shoe13 = "Campus";
+        String shoe14 = "Sparx";
+        String shoe15 = "HRX";
+        String shoe16 = "Nike India";
+        String shoe17 = "Adidas India";
+        String shoe18 = "Puma India";
+        String shoe19 = "Fila India";
+        String shoe20 = "Asian";
+        String shoe21 = "Hush Puppies India";
+        String shoe22 = "Reebok India";
+        String shoe23 = "Skechers India";
+        String shoe24 = "Ajanta";
+
+        // Storing all brands in an array
+        String[] shoes = {shoe1, shoe2, shoe3, shoe4, shoe5, shoe6, shoe7, shoe8, shoe9, shoe10, 
+                          shoe11, shoe12, shoe13, shoe14, shoe15, shoe16, shoe17, shoe18, shoe19, shoe20, 
+                          shoe21, shoe22, shoe23, shoe24};
+
+        // Printing all elements using index
+        System.out.println("Original Shoe Brands:");
+        System.out.println("Shoe Brand @ index 0: " + shoes[0]);
+        System.out.println("Shoe Brand @ index 1: " + shoes[1]);
+        System.out.println("Shoe Brand @ index 2: " + shoes[2]);
+        System.out.println("Shoe Brand @ index 3: " + shoes[3]);
+        System.out.println("Shoe Brand @ index 4: " + shoes[4]);
+        System.out.println("Shoe Brand @ index 5: " + shoes[5]);
+        System.out.println("Shoe Brand @ index 6: " + shoes[6]);
+        System.out.println("Shoe Brand @ index 7: " + shoes[7]);
+        System.out.println("Shoe Brand @ index 8: " + shoes[8]);
+        System.out.println("Shoe Brand @ index 9: " + shoes[9]);
+        System.out.println("Shoe Brand @ index 10: " + shoes[10]);
+        System.out.println("Shoe Brand @ index 11: " + shoes[11]);
+        System.out.println("Shoe Brand @ index 12: " + shoes[12]);
+        System.out.println("Shoe Brand @ index 13: " + shoes[13]);
+        System.out.println("Shoe Brand @ index 14: " + shoes[14]);
+        System.out.println("Shoe Brand @ index 15: " + shoes[15]);
+        System.out.println("Shoe Brand @ index 16: " + shoes[16]);
+        System.out.println("Shoe Brand @ index 17: " + shoes[17]);
+        System.out.println("Shoe Brand @ index 18: " + shoes[18]);
+        System.out.println("Shoe Brand @ index 19: " + shoes[19]);
+        System.out.println("Shoe Brand @ index 20: " + shoes[20]);
+        System.out.println("Shoe Brand @ index 21: " + shoes[21]);
+        System.out.println("Shoe Brand @ index 22: " + shoes[22]);
+        System.out.println("Shoe Brand @ index 23: " + shoes[23]);
+
+        // Updating all elements using index
+        shoes[0] = "UpdatedShoe1";
+        shoes[1] = "UpdatedShoe2";
+        shoes[2] = "UpdatedShoe3";
+        shoes[3] = "UpdatedShoe4";
+        shoes[4] = "UpdatedShoe5";
+        shoes[5] = "UpdatedShoe6";
+        shoes[6] = "UpdatedShoe7";
+        shoes[7] = "UpdatedShoe8";
+        shoes[8] = "UpdatedShoe9";
+        shoes[9] = "UpdatedShoe10";
+        shoes[10] = "UpdatedShoe11";
+        shoes[11] = "UpdatedShoe12";
+        shoes[12] = "UpdatedShoe13";
+        shoes[13] = "UpdatedShoe14";
+        shoes[14] = "UpdatedShoe15";
+        shoes[15] = "UpdatedShoe16";
+        shoes[16] = "UpdatedShoe17";
+        shoes[17] = "UpdatedShoe18";
+        shoes[18] = "UpdatedShoe19";
+        shoes[19] = "UpdatedShoe20";
+        shoes[20] = "UpdatedShoe21";
+        shoes[21] = "UpdatedShoe22";
+        shoes[22] = "UpdatedShoe23";
+        shoes[23] = "UpdatedShoe24";
+
+        // Printing updated shoe brands using index
+        System.out.println("\nUpdated Shoe Brands:");
+        System.out.println("Shoe Brand @ index 0: " + shoes[0]);
+        System.out.println("Shoe Brand @ index 1: " + shoes[1]);
+        System.out.println("Shoe Brand @ index 2: " + shoes[2]);
+        System.out.println("Shoe Brand @ index 3: " + shoes[3]);
+        System.out.println("Shoe Brand @ index 4: " + shoes[4]);
+        System.out.println("Shoe Brand @ index 5: " + shoes[5]);
+        System.out.println("Shoe Brand @ index 6: " + shoes[6]);
+        System.out.println("Shoe Brand @ index 7: " + shoes[7]);
+        System.out.println("Shoe Brand @ index 8: " + shoes[8]);
+        System.out.println("Shoe Brand @ index 9: " + shoes[9]);
+        System.out.println("Shoe Brand @ index 10: " + shoes[10]);
+        System.out.println("Shoe Brand @ index 11: " + shoes[11]);
+        System.out.println("Shoe Brand @ index 12: " + shoes[12]);
+        System.out.println("Shoe Brand @ index 13: " + shoes[13]);
+        System.out.println("Shoe Brand @ index 14: " + shoes[14]);
+        System.out.println("Shoe Brand @ index 15: " + shoes[15]);
+        System.out.println("Shoe Brand @ index 16: " + shoes[16]);
+        System.out.println("Shoe Brand @ index 17: " + shoes[17]);
+        System.out.println("Shoe Brand @ index 18: " + shoes[18]);
+        System.out.println("Shoe Brand @ index 19: " + shoes[19]);
+        System.out.println("Shoe Brand @ index 20: " + shoes[20]);
+        System.out.println("Shoe Brand @ index 21: " + shoes[21]);
+        System.out.println("Shoe Brand @ index 22: " + shoes[22]);
+        System.out.println("Shoe Brand @ index 23: " + shoes[23]);
+    }
+}
